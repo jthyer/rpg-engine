82a95788a4400c082ffc85320cad8e011262ad74
jthyer <john.thyer@gmail.com>
2023-05-09 16:22:58 -0500

    Combat system begins...

--- battle.lua
+++ battle.lua
@@ -3,13 +3,14 @@
 --   A battle is a kind of recursive event. In a sense, it's just a choice box that calls a new choice box continuously until the battle is over.
 --   
 --   
-
+--  Make a battle init that defines the monster parameters.
 --------------------------------------------------------- 
 function initBattleEvent()
   local event = 
     { "choiceBranch", "THE ENEMY APPROACHES!", "FIGHT", "RUN",
       {
         { "textBox", "YOU STRIKE THE FOE FOR DAMAGE!" } ,
+        { "battle" } ,
       } ,
       {
         { "textBox", "YOU FLEE!" } ,
